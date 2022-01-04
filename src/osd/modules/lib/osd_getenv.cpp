568395d8c0169cea5ee49e8f47ede0c3b956e2a7
CharlesThobe <charlesthethobe@gmail.com>
2022-01-04 04:48:24 +0200

    Use only needed functions from osdlib

---
+++ src/osd/modules/lib/osd_getenv.cpp
@@ -0,0 +1,23 @@
+// license:BSD-3-Clause
+// copyright-holders:Olivier Galibert, R. Belmont
+//============================================================
+//
+//  sdlos_*.c - OS specific low level code
+//
+//  SDLMAME by Olivier Galibert and R. Belmont
+//
+//============================================================
+
+// MAME headers
+#include "osdcore.h"
+#include "osdlib.h"
+
+//============================================================
+//  osd_getenv
+//============================================================
+
+const char *osd_getenv(const char *name)
+{
+	return getenv(name);
+}
+
