64883c012ca3bef6a65caa0b8d192ca89714bb82
CharlesThobe <charlesthethobe@gmail.com>
2022-01-04 05:45:25 +0200

    Copy only chdman.cpp from tools.

--- update_tree.bash
+++ update_tree.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+mkdir 3rdparty
 rsync -r --delete ../mame/3rdparty/{expat,libflac,lzma,nanosvg,utf8proc,zlib} 3rdparty/
-rsync -r --delete ../mame/src/{emu,frontend,lib,tools} src/
+rsync -r --delete ../mame/src/{emu,frontend,lib} src/
 rsync -r --delete --exclude="modules/lib/osd_getenv.cpp" ../mame/src/osd src/
+mkdir src/tools/
+rsync ../mame/src/tools/chdman.cpp src/tools/
+
