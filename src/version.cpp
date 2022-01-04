26369efdca2e6452bfb931034347561b195f618a
CharlesThobe <charlesthethobe@gmail.com>
2022-01-04 04:25:37 +0200

    Get the version from the CMake file

--- src/version.cpp
+++ src/version.cpp
@@ -1,6 +1,4 @@
 //Charles Bin Thobe
-#define BARE_BUILD_VERSION "0.238-CBT_edition"
-#define BARE_VCS_REVISION "mame0238-249-g6e0218c2fe6"
 extern const char bare_build_version[];
 extern const char bare_vcs_revision[];
 extern const char build_version[];
